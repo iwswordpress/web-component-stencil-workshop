b270857c3c04a3d18d7690168f65456a89c837cf
iwswordpress <iwswordpress@gmail.com>
2020-12-13 20:39:49 +0000

    form validator

--- src/components/iws-get-data/iws-get-data.tsx
+++ src/components/iws-get-data/iws-get-data.tsx
@@ -11,14 +11,37 @@ export class GetData {
   @State() gnp: string;
   @State() population: string;
   @State() name: string;
-  @State() code: string;
+  @State() code: string = '';
 
-  onFetchData(e) {
-    e.preventDefault();
-    console.log('API_KEY: ', API_KEY);
-    console.log('Get Data...');
+  @State() value: string;
+  @State() inputValid = false;
 
-    fetch(`https://wpjs.co.uk/enterprise/wp-json/enterprise/v2/get-country-data?code=GBR`)
+  handleSubmit(event: Event) {
+    event.preventDefault();
+    if (!this.inputValid) {
+      return;
+    }
+    // send data to our backend
+    this.onFetchData(this.code);
+  }
+
+  handleChange(event) {
+    this.value = event.target.value;
+    this.code = this.value;
+    if (this.code.trim().length > 0) {
+      console.log('NOT EMPTY');
+      this.inputValid = true;
+    } else {
+      console.log('EMPTY');
+      this.inputValid = false;
+    }
+  }
+
+  onFetchData(code: string) {
+    // console.log('API_KEY: ', API_KEY);
+    // console.log('Get Data...');
+
+    fetch(`https://wpjs.co.uk/enterprise/wp-json/enterprise/v2/get-country-data?code=${code}`)
       .then(res => {
         return res.json();
       })
@@ -28,34 +51,26 @@ export class GetData {
         this.name = data[0].name;
         this.code = data[0].code;
         this.population = data[0].population;
-
-        const output = `
-          ${data[0].name} - ${data[0].code}: \n
-          Population: ${data[0].population} 
-          GNP of ${data[0].gnp}
-        `;
-        console.log(output);
-        this.countryData = output;
       })
-      .catch();
+      .catch(err => console.log('Error has occured: ', err.message));
   }
 
   render() {
-    const countryAndCode = (
-      <p>
-        Country: {this.name} - {this.code}
-      </p>
-    );
     return [
       <div>
-        <form onSubmit={this.onFetchData.bind(this)}>
-          <input id="code" placeholder="Enter code..." />
-          <button type="submit">Get data</button>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <label>
+            Name:
+            <input type="text" value={this.value} onInput={event => this.handleChange(event)} />
+          </label>
+          <button type="submit" disabled={!this.inputValid}>
+            {!this.inputValid ? 'Enter code' : 'GET DATA'}
+          </button>
         </form>
       </div>,
       <div>
-        <h3>Country Data:</h3>
-        <p>{countryAndCode}</p>
+        <h3>Country Code: {this.code}</h3>
+        <h2>{this.name}</h2>
         <p>Population: {this.population}</p>
         <p>GNP: {this.gnp}</p>
       </div>,
