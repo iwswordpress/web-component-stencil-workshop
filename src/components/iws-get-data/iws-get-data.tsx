23093f2c07b5fad0cd334a0ec281bd43c7e75e93
iwswordpress <iwswordpress@gmail.com>
2020-12-14 14:33:33 +0000

    Props and input sync

--- src/components/iws-get-data/iws-get-data.tsx
+++ src/components/iws-get-data/iws-get-data.tsx
@@ -26,6 +26,8 @@ export class GetData {
     if (newValue !== oldValue) {
       console.log('Value changed: ', oldValue, newValue);
       this.value = newValue;
+      this.inputValid = true;
+      this.fetchDataOnProp(newValue);
     }
   }
 
@@ -61,6 +63,20 @@ export class GetData {
       console.log('CONF:', conf[0].population);
     });
   }
+  fetchDataOnProp(countryProp) {
+    fetch(`https://wpjs.co.uk/enterprise/wp-json/enterprise/v2/get-country-data?code=${countryProp}`)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        this.gnp = data[0].gnp;
+        this.name = data[0].name;
+        this.code = data[0].code;
+        this.population = data[0].population;
+      })
+      .catch(err => console.log('Error has occured: ', err.message));
+  }
   onFetchData() {
     // console.log('API_KEY: ', API_KEY);
     // console.log('Get Data...');
@@ -78,8 +94,15 @@ export class GetData {
       })
       .catch(err => console.log('Error has occured: ', err.message));
   }
-
-  componentDidLoad() {}
+  // initial load
+  componentDidLoad() {
+    if (this.countryProp) {
+      this.value = this.countryProp;
+      console.log('loaded', this.value);
+      this.inputValid = true;
+      this.fetchDataOnProp(this.countryProp);
+    }
+  }
   render() {
     return [
       <div>
